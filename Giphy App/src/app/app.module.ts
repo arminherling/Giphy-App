a3aaf060e7d79de4919c41575b3d62271a1f5b17
Armin Herling <armin.herling@gmail.com>
2018-04-05 16:20:25 +0200

    Changed to Angular

--- Giphy App/src/app/app.module.ts
+++ Giphy App/src/app/app.module.ts
@@ -1,21 +1,18 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    AppComponent
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    BrowserModule
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
