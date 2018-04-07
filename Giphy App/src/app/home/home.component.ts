78416c38fe7ebddd07e5004eb4a66c960a9f03e0
Armin Herling <armin.herling@gmail.com>
2018-04-07 19:39:52 +0200

    Home now loads a gif

--- Giphy App/src/app/home/home.component.ts
+++ Giphy App/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GiphyService } from '../giphy.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public gif: string;
+  public result: any;
+  public isLoading: boolean = true;
 
-  constructor() { }
+  constructor(private giphyService: GiphyService) {
+    this.getRandomGif()
+  }
 
   ngOnInit() {
   }
 
+  getRandomGif() {
+    this.giphyService.getRandomGif().subscribe(
+      (data) => {
+        this.result = data;
+        this.gif = this.result.data.image_url;
+        this.isLoading = false;
+      },
+      (err) => console.log('Ooops!', err),
+      () => console.log('Response', this.result)
+    )
+  }
 }
