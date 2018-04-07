106eef3a16970065f3aa9b1cf7211f7ed42830e1
Armin Herling <armin.herling@gmail.com>
2018-04-07 22:03:24 +0200

    The home page now shows a random gif

--- Giphy App/src/app/gif-viewer/gif-viewer.component.ts
+++ Giphy App/src/app/gif-viewer/gif-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gif-viewer',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gif-viewer.component.css']
 })
 export class GifViewerComponent implements OnInit {
+  @Input() imgUrl: string;
 
   constructor() { }
 
